47a45ea0196216773f2dbd4ba4567c93fcacca7e
chanintornc <chanintornc@sau.ac.th>
2025-02-13 00:13:03 +0700

    start project ui complete

---
+++ TravelRecSW/FrmRegister.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TravelRecSW
+{
+    public partial class FrmRegister : Form
+    {
+        public FrmRegister()
+        {
+            InitializeComponent();
+        }        
+    }
+}
